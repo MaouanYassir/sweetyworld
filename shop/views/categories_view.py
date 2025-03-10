9d3fd62756fe6ce6ab1f863b70a962515080251b
MaouanYassir <ysr1180@hotmail.com>
2025-03-10 13:09:48 +0100

    ajout des champs noms et descriptions en fr et nl aux produits et modification du templates category.html

--- shop/views/categories_view.py
+++ shop/views/categories_view.py
@@ -5,6 +5,4 @@ from shop.models import Category
 
 def categories_view(request):
     categories = Category.objects.all()
-    from django.utils.translation import get_language
-    language = get_language()  # Obtient la langue courante
-    return render(request, 'shop/categories.html', {'categories': categories, 'language': language})
+    return render(request, 'shop/categories.html', {'categories': categories})
