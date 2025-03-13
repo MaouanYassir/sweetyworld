d2ec9891fad3c1a9b4d45e0ed6e24c9a17df83fe
MaouanYassir <ysr1180@hotmail.com>
2025-03-13 18:34:22 +0100

    traduction du template CGV

--- shop/views/CGV_view.py
+++ shop/views/CGV_view.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def CGV_view(request):
     return render(request, "shop/CGV.html")
+
+
+
+#!!!!!!!!!faire la traduction des CGV!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
